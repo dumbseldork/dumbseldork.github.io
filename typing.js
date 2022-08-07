e0b2709d62c6af5f9ff45e98810fd513276eb2bc
alliah <alliahlazaro0922@gmail.com>
2022-08-08 01:46:37 +0800

    FIle Upload

---
+++ typing.js
@@ -0,0 +1,7 @@
+    var typing = new Typed (".typing" ,{
+
+        strings : ["an Electronics Engineering Student", "interested in Web Development", "interested in Cloud Engineering", "interested in DevOps Engineering"],
+        typeSpeed: 100,
+        backSpeed:60,
+        loop:true
+    });
